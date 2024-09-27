07255c077fa01459b8b7934d8270f59e59fcb9e3
roushan-sheik <roushansheik@gmail.com>
2024-09-28 01:32:58 +0600

    add: desktop navbar

--- src/components/dahboard/main/Navbar.tsx
+++ src/components/dahboard/main/Navbar.tsx
@@ -16,14 +16,36 @@ const Navbar = () => {
           />
         </div>
       </div>
-      {/* right side profile and settings container  */}
-      <div className="flex items-center gap-4">
+      {/* right side profile and settings container ================> */}
+      <div className="flex items-center gap-4 mr-12">
         {/* item 1  */}
-        <div></div>
+        <div className="flex justify-center cursor-pointer items-center border-[#FEFEFE] border size-[2.75rem] rounded-full shadow-md">
+          <Image
+            src={"/icons/endicator.png"}
+            width={22}
+            height={22}
+            alt="Icon"
+          />
+        </div>
         {/* item 2  */}
-        <div></div>
+        <div className="flex justify-center cursor-pointer items-center border-[#FEFEFE] border size-[2.75rem] rounded-full shadow-md">
+          <Image
+            src={"/icons/elements.png"}
+            width={22}
+            height={22}
+            alt="Icon"
+          />
+        </div>
         {/* item 3  */}
-        <div></div>
+        <div className="flex justify-center size-[2.75rem] overflow-hidden  items-cente rounded-full  ">
+          <Image
+            className="w-full h-full rounded-full object-cover"
+            src={"/assets/person.JPEG"}
+            width={44}
+            height={44}
+            alt="Icon"
+          />
+        </div>
       </div>
     </div>
   );
