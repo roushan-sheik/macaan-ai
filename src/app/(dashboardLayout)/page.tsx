4adf09995b385db0abea2bb7b2cf2cf7a92598cc
roushan-sheik <roushansheik@gmail.com>
2024-09-28 01:11:28 +0600

    add: customer service header dropdown

--- src/app/(dashboardLayout)/page.tsx
+++ src/app/(dashboardLayout)/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div className="">HomePage</div>;
+  return (
+    <div className="">
+      <h2>HomePage</h2>
+      <button className="btn btn-primary">Home</button>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/components/dahboard/main/Navbar.tsx
+++ src/components/dahboard/main/Navbar.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-[#FFFFFF] shadow-lg flex justify-between items-center h-[4.75rem]  ">
+      {/* customer support dropdown container */}
+      <div>
+        <div className="flex ml-8 cursor-pointer justify-between items-center bg-[#E3ECFF] border-2  h-[38px] px-3 border-[#BBCCFF] rounded-md bg-[##E3ECFF] w-[12.75rem]   ">
+          <span className="text-sm text-[#171717]">Customer Support</span>
+          <Image
+            src={"/icons/arrow-bottom.png"}
+            width={8}
+            height={8}
+            alt="Icon"
+          />
+        </div>
+      </div>
+      {/* right side profile and settings container  */}
+      <div className="flex items-center gap-4">
+        {/* item 1  */}
+        <div></div>
+        {/* item 2  */}
+        <div></div>
+        {/* item 3  */}
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
