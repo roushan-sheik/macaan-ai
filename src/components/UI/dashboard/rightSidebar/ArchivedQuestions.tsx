c8162c9c01682ba8e06c7493f4d16bd76d214ed4
roushan-sheik <roushansheik@gmail.com>
2024-09-28 11:07:25 +0600

    add: achive questions ui

--- src/components/UI/dashboard/rightSidebar/ArchivedQuestions.tsx
+++ src/components/UI/dashboard/rightSidebar/ArchivedQuestions.tsx
@@ -2,15 +2,25 @@ import React from "react";
 
 const ArchivedQuestions = () => {
   return (
-    <div>
+    <>
       {/* achive questions heading  */}
-      <div className="bg-[#FAFAFA] rounded-[10px] p-3 border border-[#E2E2E2]">
-        <h3 className="text-xs font-semibold">Archived Questions</h3>
-        <div>
+      <div className="bg-[#FAFAFA] mt-2 rounded-[10px] p-3 border border-[#E2E2E2]">
+        <div className="flex justify-between items-center mb-2">
+          <h3 className="text-xs font-semibold">Archived Questions</h3>
           <span className="text-[#3062F9] text-xs">88% Similar</span>
         </div>
+        {/* answer content  */}
+        <h4 className="text-[#050D1F] text-xs font-semibold">
+          Q: Order to help you locate the correct parts and repair{" "}
+        </h4>
+        <p className="mt-1 text-[#050D1F]">
+          Order to help you locate the correct parts and repair information, we
+          will need more information about the problem you are facing. Please
+          submit a new question with more details, and we will be happy to help
+          you. Looking forward to hearing from you!
+        </p>
       </div>
-    </div>
+    </>
   );
 };
 
