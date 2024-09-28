356bc57de23d3176811229ee8de4e37587c48d0c
roushan-sheik <roushansheik@gmail.com>
2024-09-28 14:58:11 +0600

    add: responsive replay form ui

--- src/components/UI/dashboard/main/ReplayMessageForm.tsx
+++ src/components/UI/dashboard/main/ReplayMessageForm.tsx
@@ -12,13 +12,13 @@ const ReplayMessageForm = () => {
       </h2>
       <form action="#">
         {/* input box parent =====================> */}
-        <div className="flex items-center justify-between gap-8">
+        <div className="flex items-center flex-col lg:flex-row justify-between gap-8">
           {/* box- 1 */}
-          <div className="w-6/12">
+          <div className="lg:w-6/12 w-full">
             <h3 className="text-[#4F4F4F] mb-1">URL</h3>
             <input
               type="text"
-              className="input input-bordered border-[#DFDFDF] input-md w-full max-w-xs"
+              className="input input-bordered border-[#DFDFDF] input-md w-full "
             />
             {/* tags  */}
             <div className="mt-4 flex items-center justify-between">
@@ -28,11 +28,11 @@ const ReplayMessageForm = () => {
             </div>
           </div>
           {/* box- 2 */}
-          <div className="w-6/12">
+          <div className="lg:w-6/12 w-full">
             <h3 className="text-[#4F4F4F] mb-1">Related Parts</h3>
             <input
               type="text"
-              className="input input-bordered border-[#DFDFDF]  input-md w-full max-w-xs"
+              className="input input-bordered border-[#DFDFDF]  input-md w-full  "
             />
             {/* tags  */}
             <div className="mt-4 flex items-center justify-between">
@@ -50,13 +50,13 @@ const ReplayMessageForm = () => {
             className="input  input-bordered border-[#DFDFDF] input-lg w-full "
           />
           {/* button and tags parent  */}
-          <div className="flex items-center justify-between mt-4">
-            <div className="flex w-6/12 justify-between ">
+          <div className="flex flex-col lg:flex-row items-center justify-between mt-4">
+            <div className="flex lg:w-6/12 w-full justify-between ">
               <CommentTempTag />
               <CommentTempTag />
               <CommentTempTag />
             </div>
-            <div className="w-6/12 flex justify-end">
+            <div className="lg:w-6/12 mt-8 lg:mt-0 w-full flex lg:justify-end justify-center">
               <button className="rounded-lg bg-[#2B68E8] px-8 py-3 text-xl text-white duration-300 active:scale-95">
                 Post Comment
               </button>
