3f29adb7aba32c0a712bdea3f3af18d78955504e
roushan-sheik <roushansheik@gmail.com>
2024-09-28 14:52:01 +0600

    add: replay message form ui

---
+++ src/components/UI/dashboard/main/CommentTemp.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CommentTempTag = () => {
+  return (
+    <div className="text-[#091323] font-medium cursor-pointer bg-[#F6F6F6] border-[#BBCCFF] border-[1px] rounded-md py-[8px] px-[10px]">
+      Temp 1
+    </div>
+  );
+};
+
+export default CommentTempTag;
--- src/components/UI/dashboard/main/ReplayMessageForm.tsx
+++ src/components/UI/dashboard/main/ReplayMessageForm.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import React from "react";
 import Tag from "./Tag";
+import CommentTempTag from "./CommentTemp";
 
 const ReplayMessageForm = () => {
   return (
@@ -10,14 +11,14 @@ const ReplayMessageForm = () => {
         Compose your reply
       </h2>
       <form action="#">
-        {/* input box parent  */}
+        {/* input box parent =====================> */}
         <div className="flex items-center justify-between gap-8">
           {/* box- 1 */}
           <div className="w-6/12">
             <h3 className="text-[#4F4F4F] mb-1">URL</h3>
             <input
               type="text"
-              className="input input-bordered input-md w-full max-w-xs"
+              className="input input-bordered border-[#DFDFDF] input-md w-full max-w-xs"
             />
             {/* tags  */}
             <div className="mt-4 flex items-center justify-between">
@@ -31,13 +32,34 @@ const ReplayMessageForm = () => {
             <h3 className="text-[#4F4F4F] mb-1">Related Parts</h3>
             <input
               type="text"
-              className="input input-bordered input-md w-full max-w-xs"
+              className="input input-bordered border-[#DFDFDF]  input-md w-full max-w-xs"
             />
             {/* tags  */}
             <div className="mt-4 flex items-center justify-between">
               <Tag />
               <Tag />
               <Tag />
+            </div>
+          </div>
+        </div>
+        {/* ansers input  parent =======================>   */}
+        <div className="mt-6">
+          <p className="text-[#4F4F4F] mb-2">Your answer goes here</p>
+          <input
+            type="text"
+            className="input  input-bordered border-[#DFDFDF] input-lg w-full "
+          />
+          {/* button and tags parent  */}
+          <div className="flex items-center justify-between mt-4">
+            <div className="flex w-6/12 justify-between ">
+              <CommentTempTag />
+              <CommentTempTag />
+              <CommentTempTag />
+            </div>
+            <div className="w-6/12 flex justify-end">
+              <button className="rounded-lg bg-[#2B68E8] px-8 py-3 text-xl text-white duration-300 active:scale-95">
+                Post Comment
+              </button>
             </div>
           </div>
         </div>
